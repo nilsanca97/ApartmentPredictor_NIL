add46be26cb8b759cc48795f406cf779dd0226a0
nil.sanca97 <nil.sanca97@gmail.com>
2025-12-04 12:23:47 +0100

    change name repo from ApartmentPredictor to 'ApartmentPredictorSpringBoot' to differentiate it from root .git, that contains docs, ApartmentPredictosSpringBoot, db, etc

---
+++ ApartmentPredictorSpringBoot/src/main/java/com/example/apartment_predictor/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apartment_predictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ ApartmentPredictorSpringBoot/src/test/java/com/example/apartment_predictor/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apartment_predictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
